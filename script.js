0151d53869b482f9bc41ea3c1d8542dca22a9919
Bkaur90 <baljeetkaler3019@gmail.com>
2026-07-13 12:12:55 -0400

    Added JS

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function SearchRecipe(event) {
+  event.preventDefault();
+  new Typewriter(".recipe-container", {
+    strings: "Recipe content will be displayed here",
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#recipe-form");
+formElement.addEventListener("submit", SearchRecipe);
 
